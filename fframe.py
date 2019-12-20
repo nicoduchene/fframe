29268cf4e840af884136019ab8f246ca59e16e55
Nicolas Duchene <nicoduc@localhost.localdomain>
2019-12-19 20:50:36 -0500

    took off inconsistencies and added functional method

--- fframe.py
+++ fframe.py
@@ -8,7 +8,6 @@ class FFrame(object):
         domain_min (float): Minimum of the domain.
         domain_max (float): Maximum of the domain.
         func (:obj:callable): Function to discretize.
-        funcsteps (int, optional): Number of steps to interpolate function on. Defaults to 100.
         outside the domain. Defaults to True.
         domain_gran (float): Domain granularity. Defaults to 5.
         image_gran (float): Image granularity. Defaults to 0.5.
@@ -25,7 +24,6 @@ class FFrame(object):
 
     
     def __init__(self, domain_min, domain_max, func,
-                funcsteps=100,
                 domain_gran=5, image_gran=0.5,
                 ):
 
@@ -35,21 +33,26 @@ class FFrame(object):
         self.func = func
         self.domain_min = domain_min
         self.domain_max = domain_max
-        self.funcsteps = funcsteps
 
         domain = np.arange(start=domain_min, stop=domain_max+self.domain_gran, step=self.domain_gran)
         self.domain = domain
-        values = self.func(self.domain)
 
-        self.func_domain = np.linspace(self.domain_min, self.domain_max, num=self.funcsteps)
         
         # Define the allowed values given the chosen granularities
-        max_val = np.max(values)
-        if extrapolate: max_val += self.image_gran
-        min_val = round(np.min(values) / self.image_gran) * self.image_gran
-        allowed_values = np.arange(start=min_val, stop=max_val+self.image_gran, step=self.image_gran)
-        self.discrete_func = self.chunk(allowed_values, values)
 
+    def functional_method(self):
+        for x in self.domain:
+            yield np.round(self.func(x) / self.image_gran) * self.image_gran
+
+    def lut_method(self):
+
+        values = self.func(self.domain)
+        max_val = np.max(values)
+        # if extrapolate: max_val += self.image_gran
+        min_val = np.round(np.min(values) / self.image_gran) * self.image_gran
+        allowed_values = np.arange(start=min_val, stop=max_val+self.image_gran, step=self.image_gran)
+        return self.chunk(allowed_values, values)
+        
     def chunk(self, allowed_values, values, steps=10):
         """ Round values to nearest allowed_value. """
 
@@ -99,20 +102,24 @@ class FFrame(object):
         
         return np.array(agglomerated_steps), np.array(agglomerated_values)
 
-    def compare_functions(self, ax=None, show=True):
+    def compare_functions(self, x_values, ax=None, show=True):
         """Plot the discretized and original versions of the function.
         Returns:
             ax (:obj:axis): Axis object.
         """
         
-        interp_image = self.func(self.func_domain)
+        discrete_func = self.lut_method()
+        interp_image = self.func(x_values)
+        functional_vals = list(self.functional_method())
 
         if ax:
-            ax.plot(self.domain, self.discrete_func, label='discretized', color='b')
-            ax.plot(self.func_domain, interp_image, label='interpolated', color='r')
+            ax.plot(self.domain, discrete_func, label='lut', color='b')
+            ax.plot(self.domain, functional_vals, label='functional', color='k')
+            ax.plot(self.func_domain, interp_image, label='Input values', color='r')
         else:
-            plt.plot(self.domain, self.discrete_func, label='discretized', color='b')
-            plt.plot(self.func_domain, interp_image, label='interpolated', color='r')
+            plt.plot(self.domain, discrete_func, label='lut', color='b')
+            plt.plot(self.domain, functional_vals, label='functional', color='k')
+            plt.plot(self.func_domain, interp_image, label='Input values', color='r')
 
         ax = plt.gca()
             
@@ -122,8 +129,23 @@ class FFrame(object):
         
         return ax
 
-if __name__ == "__main__":
 
-    f = FFrame(0, 120, lambda x:10*np.sin(x),
-               funcsteps=1000, domain_gran=1)
-    f.compare_functions(show=True)
+if __name__ == "__main__":
+    from timerit import Timerit
+
+    f = FFrame(0, 120, lambda x:x**3, domain_gran=0.5, image_gran=100)
+
+    # f.compare_functions(np.linspace(0,120))
+
+    if True:
+        print("Functional method:")
+        for _ in Timerit(num=100, verbose=2):
+            f.functional_method()
+
+        print("\nLUT method:")
+        for _ in Timerit(num=100, verbose=2):
+            f.lut_method()
+
+    # t = Timer(f.functional_method, number=1000)
+    # print(t)
+    # f.lut_method()
